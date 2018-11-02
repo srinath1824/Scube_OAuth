095c71c1219195de262e15b34fdc07146bb1017f
Srinath <37976200+srinath1824@users.noreply.github.com>
2018-11-02 10:10:43 +0530

    Update keys.js

--- configs/keys.js
+++ configs/keys.js
@@ -3,13 +3,13 @@
 module.exports = {
   google: {
     clientID:
-      "344230026974-buhqk53avs60q7uubormn09ra0fdpu7r.apps.googleusercontent.com",
-    clientSecret: "ijoau1QbQsmCqPjJqw_OHQBu"
+      "",
+    clientSecret: ""
   },
   mongodb: {
-    dbURI: "mongodb://scube:scube123@ds261470.mlab.com:61470/scube"
+    dbURI: ""
   },
   session: {
-    cookieKey: "Scubeisthebest"
+    cookieKey: ""
   }
 };
