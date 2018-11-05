916701ba21e8add10fdef226cc562a47b15a71e4
Srinath <37976200+srinath1824@users.noreply.github.com>
2018-11-05 14:57:57 +0530

    Update passport-setup.js

--- configs/passport-setup.js
+++ configs/passport-setup.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
+const FacebookStrategy = require("passport-facebook");
 const keys = require("./keys");
 const User = require("../models/user-model");
 
@@ -42,3 +43,29 @@ passport.use(
     }
   )
 );
+
+passport.use(new FacebookStrategy({
+  callbackURL: "/auth/facebook/redirect",
+  clientID: keys.facebook.clientID,
+  clientSecret: keys.facebook.clientSecret
+},
+(accessToken, refreshToken, profile, done) => {
+  User.findOne({ 'facebook.id': profile.id }).then(currentUser => {
+    if (currentUser) {
+      //already have user
+      done(null, currentUser);
+    } else {
+      let newUser = new User()
+      newUser.facebook.name= profile.name.givenName + " " + profile.name.familyName;
+      newUser.facebook.id = profile.id;
+      newUser.facebook.token = accessToken;
+      newUser.facebook.email = profile.emails[0].value;
+      newUser.save()
+        .then(newUser => {
+          console.log("new user was created" + newUser);
+          done(null, newUser);
+        });
+    }
+  });
+}
+));
